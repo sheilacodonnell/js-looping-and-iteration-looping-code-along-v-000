d6d75599ec036dd705fe7d2d4df04bf1fd7217aa
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-19 18:33:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function writeCards(names, occasion) {
   for (let i = 0; i < names.length; i++){
     console.log(`Thank you ${names[i]} for the wonderful ${occasion} gift!`);
   }
+  return names;
+  return occasion;
 }
 
+writeCards(names, occasion);
