1a54d598519ded03d28a60a89863f5beb0737a14
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-19 18:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const names = [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) 
-const occasion =['surprise'];
 
 function writeCards(names, occasion) {
   for (let i = 0; i < names.length; i++){
-    var cards = (`Thank you, ${names[i]} for the wonderful ${names[3]} gift!`);
+    (`Thank you, ${names[i]} for the wonderful ${names[3]} gift!`);
   }
-  return cards;
+  return names;
 }
 
-writeCards(names, occasion);
+writeCards(names);
