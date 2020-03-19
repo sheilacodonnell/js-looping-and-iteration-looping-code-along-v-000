3ca7751a638a8aa58c6e79593e828bb379237f4e
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-19 19:16:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const occasion = 'surprise';
+const cards = [];
+
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++){
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return cards;
+}
+
+
+function countDown(number) {
+  while ( number >= 0) {
+    console.log(number--);
+  }
+}
